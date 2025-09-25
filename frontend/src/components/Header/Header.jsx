a4d82c1c08b71b188ee339cb9b996898fa40abc2
Angelina-Hryachko <angelina.hryachko@gmail.com>
2025-09-25 22:56:08 +0300

    add header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your favourite food here</h2>
+            <h2>Замовте тут свою улюблену їжу</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission to satisfy your craving and elevate your donong experience, one delisious meal at a time.</p>
             <button>View Menu</button>
         </div>
