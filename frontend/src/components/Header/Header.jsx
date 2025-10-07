97a63e7b9d4334544057664f8df4024d5221a53a
Angelina-Hryachko <angelina.hryachko@gmail.com>
2025-10-07 16:56:50 +0300

    fix nav button

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -7,7 +8,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favorite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission to satisfy your craving and elevate your donong experience, one delisious meal at a time.</p>
-            <button>View Menu</button>
+            <Link to={'#explore-menu'}>View Menu</Link>
         </div>
     </div>
   )
