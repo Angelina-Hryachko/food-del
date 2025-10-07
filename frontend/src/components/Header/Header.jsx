496c7262d1dc1ed0c71584d2f84b91d168646b24
Angelina-Hryachko <angelina.hryachko@gmail.com>
2025-10-07 17:06:06 +0300

    add header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Header.css'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -8,7 +7,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favorite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission to satisfy your craving and elevate your donong experience, one delisious meal at a time.</p>
-            <Link to={'#explore-menu'}>View Menu</Link>
+            <a href='#explore-menu' className='button'>View Menu</a>
         </div>
     </div>
   )
